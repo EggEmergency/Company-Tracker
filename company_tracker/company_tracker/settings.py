ffc79675a91525c958574649ea73da97c9f106f9
Christian Lee <christian.lee93@gmail.com>
2020-05-14 15:14:14 -0400

    Landing page displays information from the database in a datatable

--- company_tracker/company_tracker/settings.py
+++ company_tracker/company_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'landing_page.apps.LandingPageConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'company_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- company_tracker/landing_page/views.py
+++ company_tracker/landing_page/views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 
 from .models import Company
 def landingView(request):
-    """
     allCompanies = Company.objects.all()
     context = {
         'company_list': allCompanies,
     }
     return render(request, 'landing_page.html', context)
-    """
-    return HttpResponse("LandingView baby")
